95c411278cc992d816cb83e42b3c6f3c19b590be
YehorMajboroda <egormajboroda4@gmail.com>
2026-04-05 03:02:20 +0300

    Introduce aggregation class Wardrobe

---
+++ lab4/src/main/java/com/yehor/lab4/Wardrobe.java
@@ -0,0 +1,26 @@
+package com.yehor.lab4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Клас Wardrobe агрегує Clothes
+ */
+public class Wardrobe { // +++
+
+    private List<Clothes> clothesList = new ArrayList<>(); // +++
+
+    public void addClothes(Clothes c) { // +++
+        clothesList.add(c);
+    }
+
+    public void showAll() { // +++
+        if (clothesList.isEmpty()) {
+            System.out.println("Список порожній!");
+        } else {
+            for (Clothes c : clothesList) {
+                System.out.println(c);
+            }
+        }
+    }
+}
